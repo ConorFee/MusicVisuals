eff2cd3001c35e459ba09ceaa7cecc1d1818981b
Conor <C22414306@mytudublin.ie>
2024-03-19 12:53:19 +0000

    test commit

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -5,11 +5,13 @@ import example.MyVisual;
 import example.RotatingAudioBands;
 
 public class Main {
-
+    
     public void startUI() {
         String[] a = { "MAIN" };
         processing.core.PApplet.runSketch(a, new MyVisual());
     }
+
+    // test commit..
 
     public static void main(String[] args) {
         Main main = new Main();
