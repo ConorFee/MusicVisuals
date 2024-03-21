830fd47b0f1a2ac3100d81e9dfeb05672770aa49
C22517166 <c22517166@mytudublin.ie>
2024-03-21 11:49:43 +0000

    Starter Files Moved to java/src/C22414306/

---
+++ java/src/C22414306/Main.java
@@ -0,0 +1,20 @@
+package C22414306;
+
+import example.CubeVisual;
+import example.MyVisual;
+import example.RotatingAudioBands;
+
+public class Main {
+    
+    public void startUI() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new MyVisual());
+    }
+
+    // test commit..
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.startUI();
+    }
+}
