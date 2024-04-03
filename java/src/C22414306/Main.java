7047bb9656840d2ae2852c8518c65822485e7416
marklangan <c22470414@mytudublin.ie>
2024-04-03 13:57:32 +0100

    g

--- java/src/C22414306/Main.java
+++ java/src/C22414306/Main.java
@@ -22,3 +22,22 @@ public class Main {
         
     }
 }
+
+public class Main {
+    
+    public void startUI() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new MyVisual());
+    }
+
+    // test commit..
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.startUI();
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
