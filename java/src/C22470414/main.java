f0a946fcef148fca10beb9cbcfc5cff14f34c74f
marklangan <c22470414@mytudublin.ie>
2024-04-17 14:45:26 +0100

    basic visuals

--- java/src/C22470414/main.java
+++ java/src/C22470414/main.java
@@ -1,7 +1,26 @@
 package C22470414;
 
-import ie.tudublin.Visual;
 
-public class main {
-    
+public class Main
+{
+
+	public void helloProcessing()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Sound());
+    }
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+		
+		
+		Main m = new Main();
+	
+
+		m.helloProcessing();
+	}
+	
 }
+
+
