9821b26e683875d15cfa40d017537364be4df49f
C22517166 <c22517166@mytudublin.ie>
2024-04-18 17:10:54 +0100

    bla

--- java/src/example/Main.java
+++ java/src/example/Main.java
@@ -7,7 +7,7 @@ public class Main
    public void helloProcessing()
    {
        String[] a = {"MAIN"};
-       processing.core.PApplet.runSketch( a, new CubeVisual());
+       processing.core.PApplet.runSketch( a, new CubeVisual1());
    }
    
    public static void main(String[] args)
