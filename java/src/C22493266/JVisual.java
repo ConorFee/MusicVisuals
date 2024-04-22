4c81549fdcd4b63252f9f30e7f7e5b04ba2fd718
jake walsh <jakejwalsh1@gmail.com>
2024-04-22 20:41:42 +0100

    added green fields, click to place clouds , and  few more comments

--- java/src/C22493266/JVisual.java
+++ java/src/C22493266/JVisual.java
@@ -1,20 +1,26 @@
 package C22493266;
 
+import java.util.ArrayList;
+
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
+import processing.core.PVector;
+import java.util.ArrayList;
 
 public class JVisual extends PApplet {
 
     Minim m;
     AudioInput ai;
     AudioPlayer ap;
-    
     AudioBuffer b;
 
     float circleSize;       //put circlesize up here as i was having trouble accessing it in the clouds function
+
+    ArrayList<PVector> cloudPostions;       // this list stores clicked locations for clouds
+
 
     @Override
     public void settings() {
@@ -26,34 +32,33 @@ public class JVisual extends PApplet {
         m = new Minim(this);
         ap = m.loadFile("stayinit.mp3");
         //ai = m.getLineIn(Minim.MONO, width, 44100, 16);
-
         ap.play();
         b = ap.mix;
+
+        cloudPostions = new ArrayList<>(); //initialize list
+
     }
 
     @Override
     public void draw() {
 
              
-             setGradient(0, 0, width, height, color(137, 255, 150), color(137, 255, 240)); // fades from light to darker blue sky
+             setGradient(0, 0, width, height, color(137, 255, 180), color(137, 255, 255)); // fades from light to darker blue sky
 
 
+
+             drawFields();
              cloudShadow();
              clouds();
              
 
 
              float avg = 0; 
-             
              for (int i = 0; i < b.size(); i++){
-
                 avg += abs(b.get(i));   
-
              }
-
              avg /= b.size();       //average creation complete at this point, measured sound
-
-             circleSize = map((avg),0,1,50,300); //size rangeees
+             circleSize = map(avg,0,1,50,300); //size rangeees
 
 
              colorMode(HSB);
@@ -67,19 +72,34 @@ public class JVisual extends PApplet {
 
 
              stroke(hue,255,255);
-             int numLines = 199;
+             int numLines = 20;
              float angles =  TWO_PI / numLines;
+
+            
+             
+
+
+
+
              for (int i = 0; i < numLines; i++) {
                 float lineLength = map(b.get(i % b.size()), -1, 1,0,150);       //https://processing.org/examples/sinecosine.html
                 float x1 = width/2 + cos(angles * i) * (circleSize / 2) ;
                 float y1 = height/2 + sin(angles * i ) * (circleSize / 2 );
                 float x2 = width/2 + cos(angles * i) * (circleSize / 2 + lineLength) ;
                 float y2 = height/2 + sin(angles * i ) * (circleSize / 2 + lineLength);
+                
+                
 
                 line(x1,y1,x2,y2);
 
              }
         }
+
+
+        public void mousePressed() {
+            cloudPostions.add(new PVector(mouseX,mouseY));  //add pos of mouse click
+        }
+
 
 
         private void setGradient(int x, int y, float w, float h, int c1, int c2) {          //used for the fading sky, draws lines repeatedly, slightly changing colour as it increments
@@ -92,7 +112,10 @@ public class JVisual extends PApplet {
             }
         }
 
-
+        private void drawFields() {
+            fill(84,179,199);
+            rect(0, height * 0.8f, width, height * 0.2f) ;
+         }
 
 
 
@@ -100,6 +123,18 @@ public class JVisual extends PApplet {
             fill(255);
             noStroke();
             int DECRET = 30;            // variable i use to decrease the size of the clouds/cirlces as it uses the same sizing as the sun
+
+            for(PVector pos : cloudPostions){
+                ellipse(pos.x, pos.y, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x - 20, pos.y + 0, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x + 5, pos.y - 5, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x + 2, pos.y + 5, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x + 15, pos.y + 0, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x - 10, pos.y + 3, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x - 10, pos.y - 3, circleSize - DECRET, circleSize - DECRET);
+
+            }
+
 
             //top left clouds
             ellipse(130, 100, circleSize-DECRET, circleSize-DECRET);
@@ -131,6 +166,22 @@ public class JVisual extends PApplet {
             fill(0);
             noStroke();
             int DECRET = 20;
+
+            for(PVector pos : cloudPostions){
+                //ellipse(pos.x, pos.y, circleSize - DECRET, circleSize - DECRET);
+                //ellipse(pos.x + random(-20, 20), pos.y + random(-20, 20), circleSize - DECRET, circleSize - DECRET);
+                //ellipse(pos.x + random(-20, 20), pos.y + random(-20, 20), circleSize - DECRET, circleSize - DECRET);      //this was an attempt to randomize the shape of each cloud when clicked but it reset evey frames
+                ellipse(pos.x, pos.y, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x - 20, pos.y + 0, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x + 5, pos.y - 5, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x + 2, pos.y + 5, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x + 15, pos.y + 0, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x - 10, pos.y + 3, circleSize - DECRET, circleSize - DECRET);
+                ellipse(pos.x - 10, pos.y - 3, circleSize - DECRET, circleSize - DECRET);
+
+            }
+
+
 
             //top left clouds
             ellipse(130, 100, circleSize-DECRET, circleSize-DECRET);
