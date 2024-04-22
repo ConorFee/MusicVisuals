e0f4422b934aa689a44e387c1b0dc0d11c28eef1
jake walsh <jakejwalsh1@gmail.com>
2024-04-22 16:17:12 +0100

    experiment with RGB

--- java/src/C22493266/JVisual.java
+++ java/src/C22493266/JVisual.java
@@ -35,6 +35,7 @@ public class JVisual extends PApplet {
     @Override
     public void draw() {
         colorMode(HSB);
+        //colorMode(RGB, 1500,100,255);
         background(0);
         stroke(255);
         /* 
@@ -59,10 +60,13 @@ public class JVisual extends PApplet {
             float h = height /2;
             for(int i = 0; i < b.size() ; i++)
             {
-                float hue = map(i , 0, b.size(), 0 , 256);
-                stroke(hue, 255, 255);
-                line(mouseX , mouseY, i , h+b.get(i) * h);
-                //line(i , h + b.get(i) * h, i , h+b.get(i) * h);
+                float hue = map( i , 0, b.size(), 0 , 130);
+                stroke(mouseX, 255, 255);
+                //stroke(hue, 255, 255);
+                //fill(255, 100, 200, 25);
+                //fill(mouseX, 100, mouseY, 25);
+                //line(mouseX , mouseY, i , h+b.get(i) * h);
+                line(i , h + b.get(i) * h, i , h+b.get(i) * h);
 
             }
             
