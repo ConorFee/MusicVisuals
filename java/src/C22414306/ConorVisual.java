2491a459d4683ed10210eb8494bfd08329101879
Conor <C22414306@mytudublin.ie>
2024-04-22 15:43:35 +0100

    Draw method added, variable initalisation and declaration

--- java/src/C22414306/ConorVisual.java
+++ java/src/C22414306/ConorVisual.java
@@ -47,7 +47,7 @@ public class ConorVisual extends Visual
 
     public void setup()
     {
-        
+
         minim = new Minim(this);
         ap = minim.loadFile("stayinit.mp3", 1024);
         ap.play();
@@ -61,6 +61,48 @@ public class ConorVisual extends Visual
         
     }
 
-   
+    float off = 0;
+    float smoothedAverage = 0;
+    float x = 0;
+
+   public void draw()
+   {
+        // set colour to black
+        //
+        background(0);
+        float halfH = height / 2;
+        float average = 0;
+        float sum = 0;
+        off += 1;
+        // loop through each sample in the audio buffer
+        //
+        for(int i = 0 ; i < ab.size() ; i ++)
+        {
+            sum += abs(ab.get(i));
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
+        }
+        // calculate the average of the sampples
+        //
+        average= sum / (float) ab.size();
+        // Smooth the average value over time using the lerp function
+        //
+        smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
+        // calculate the centre point of the visualisation
+        //
+        float cx = width / 2;
+        float cy = height / 2;
+
+        switch (mode) 
+        {
+            case 1:
+                
+
+                break;
+        
+            default:
+                break;
+        }
+
+   }
 
 }
