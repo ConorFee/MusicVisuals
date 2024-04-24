339c1dfccf3a5b40b214fcb4ea9975d2eaf58d10
C22517166 <c22517166@mytudublin.ie>
2024-04-24 14:59:43 +0100

    bla

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,16 +1,10 @@
 package ie.tudublin;
 
-<<<<<<< HEAD
 import C22414306.ConorVisual;
 import C22470414.test;
 import C22493266.JVisual;
 import C22517166.Sound1;
-import example.MyVisual;
 import processing.core.PApplet;
-//import stayinit.stayinit;
-=======
-import stayinit.stayinit;
->>>>>>> bf6e776 (bla)
 
 public class Main extends PApplet {
 
